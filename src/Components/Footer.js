04281e13b5feeeb350bfcd2f5b1453021ffb255e
unknown <alicebe.phan@gmail.com>
2022-01-14 04:26:04 -0600

    Was able to remove the bullet and center the icon

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
         <div>
             <footer className='footer'>
                 <div className='icons'>
-                    <ul className='icons-list list-inline text-center'>
+                    <ul className='list-style: none list-inline text-center'>
                         <li className='list-inline-item'>
                         <a target="_blank" href="https://github.com/thutuephan"><i className="fab fa-github-square fa-3x"></i></a>
                         </li>

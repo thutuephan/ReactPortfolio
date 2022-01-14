d34417c54b4c1093763fcc1bf2cc4b28678fa2f7
unknown <alicebe.phan@gmail.com>
2022-01-14 03:00:11 -0600

    Added footer, installed prettier

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import PortfolioContainer from './components'
+import Footer from 'react'
 
 
 const App = () => {
+  const styles = {
+    backgroundColor: "#A0E7E5",
+    color: "#FFAEBC",
+    
+
+  }
+
   return (
     <div>
       <main>
-        <PortfolioContainer />
+        <PortfolioContainer styles={styles} />
 
       </main>
       <Footer />
---
+++ src/Components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div>
+            <footer className='footer mt-auto text-center'>
+                <p> Created by Alice</p>
+
+            </footer>
+            
+        </div>
+    )
+}
+
+export default Footer
